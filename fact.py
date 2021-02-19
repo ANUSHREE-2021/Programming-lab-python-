f161eb722baaddac04aba137d85dbb2a6060df5b
ANUSHREE-2021 <75942336+ANUSHREE-2021@users.noreply.github.com>
2021-02-19 18:20:35 +0530

    Delete fact.py

--- fact.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter the number: "))
-fact = 1
-if num < 0:
-    print("number is negative,factorial cannot be found")
-else:
-    for n in range(1, num+1):
-        fact = fact * n
-    print("Factorial =",fact )
