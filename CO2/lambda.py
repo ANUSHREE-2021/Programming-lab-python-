ed04468daaf75e3ab663a741458a12e7289ce84b
ANUSHREE-2021 <75942336+ANUSHREE-2021@users.noreply.github.com>
2021-02-19 18:01:27 +0530

    Add files via upload

---
+++ CO2/lambda.py
@@ -0,0 +1,9 @@
+
+square_area = lambda a : a*a
+print("Area of square: ",square_area(1))
+
+rect_area = lambda lenght,breadth : lenght * breadth
+print("Area of rectangle: ",rect_area(2,3))
+
+triangle_area = lambda b,h : 1/2 * b * h
+print("Area of triangle : ",triangle_area(1,2))
---
+++ CO2/sum_of_items.py
@@ -0,0 +1,5 @@
+li = [12,3,43,4,5,11,60]
+sum=0
+for x in range(0,len(li)):
+    sum = sum + li[x]
+print("sum of items in list = ",sum)
