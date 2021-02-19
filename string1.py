c0bdf26d958fe1f3c629afc59dae7563b65571fc
ANUSHREE-2021 <75942336+ANUSHREE-2021@users.noreply.github.com>
2021-02-19 18:25:18 +0530

    Delete string1.py

--- string1.py
+++
@@ -1,6 +0,0 @@
-string1=input("enter the first string: ")
-string2=input("enter the second string: ")
-a=string1[0:1]
-string1=string1.replace(string1[0:1],string2[0:1])
-string2=string2.replace(string2[0:1],a)
-print(string1,string2)
